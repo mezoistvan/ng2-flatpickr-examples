cc7da3d98702efcdbceada9ff4125ee269bc2f8f
mezoistvan <mezoistvan1@gmail.com>
2017-03-07 21:45:52 +0100

    initial demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Ng2FlatpickrComponent } from 'ng2-flatpickr/ng2-flatpickr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ng2FlatpickrComponent
   ],
   imports: [
     BrowserModule,
